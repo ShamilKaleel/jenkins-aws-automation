04d238e22c63b92e268686377a553dcfac081763
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-04-05 18:03:07 +0530

    change the aws_region

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,5 @@
-aws_region    = "us-east-1"
-ami_id        = "ami-0c7217cdde317cfec"  # Ubuntu 22.04 LTS in us-east-1
+aws_region    = "ap-south-1"
+ami_id        = "ami-03f4878755434977f"  # Ubuntu 22.04 LTS in ap-south-1
 instance_type = "t2.micro"               # More cost-effective option
 key_name      = "blog-app"           # Using your existing key
 ssh_user      = "ubuntu"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS in us-east-1
+  default     = "ami-03f4878755434977f" # Ubuntu 22.04 LTS in ap-south-1
 }
 
 variable "instance_type" {
